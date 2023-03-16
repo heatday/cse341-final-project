c14ca16ec93b08d7188112f6f76ae2d0314ed90e
Daniel Muzo <luiscarloespi@gmail.com>
2023-03-15 20:08:19 -0500

    Changes

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+const db  = mongoose.connection;
+db.on('error', (error ) => console.error(error))
+db.once('open', () => console.log('Connected to Database'));
 
 const port = process.env.PORT || 8080;
 
