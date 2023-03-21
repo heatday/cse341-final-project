f08f414e9d85f404e83ce33def605d1783c1ecb8
Daniel Muzo <luiscarloespi@gmail.com>
2023-03-20 23:35:57 -0500

    edit

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,7 +1,7 @@
 const authorize = (req, res, next) => {
     if (!req.oidc.isAuthenticated()) {
       return res.status(401).send({
-        error: 'You are not authorized without login.',
+        error: 'You are not authorized without login. First log in or register with this link',
         authorize: "cse341-final-project-26gf.onrender.com/login"
       });
     }
