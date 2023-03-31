de220d8355bda5ac4eecd5f2f93f4911fed83b67
ticomware <cbialo2@outlook.com>
2023-03-31 14:03:34 -0600

    attempt 3 at fixing edit comment

--- controllers/forum.js
+++ controllers/forum.js
@@ -125,14 +125,16 @@ const editCommentOnForum = async (req, res) => {
     if (!req.body.content)
       res.status(400).send({ message: 'Error: content is required.' });
 
-    forum.findOne({_id: req.params.forumId, "comments._id": req.params.commentId}, function(err, oldComment){
-      if(oldComment.author != req.oidc.user.sub)
+    forum.findOne({_id: req.params.forumId}, function(err, forumThread){
+      forumThread.comments.findOne({_id: req.params.commentId}, function(err, oldComment){
+        if(oldComment.author != req.oidc.user.sub)
           res.status(400).send({ message: 'Error: You are not allowed to edit another user\'s comment.' });
         else {
           oldComment.content = req.body.content;
           oldComment.isEdited = true;
           oldComment.save().then((data) => res.status(201).send(data)).catch((err) => res.status(500).json({message: err.message || 'An error occurred.'}));
         }
+      })
     });
   } 
   catch (err) {
