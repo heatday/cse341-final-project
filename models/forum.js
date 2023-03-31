34b8484042971e4564ae81f69619864476a9fd54
ticomware <cbialo2@outlook.com>
2023-03-31 12:49:35 -0600

    Fixed comment ID issue

--- models/forum.js
+++ models/forum.js
@@ -7,7 +7,6 @@ const forumSchema = new mongoose.Schema ({
   date: { type: Date, required: true },
   isEdited: {type: Boolean, required: false},
   comments: [{
-    id: { type: String, required: true },
     content: { type: String, required: true },
     author: { type: String, required: true },
     date: { type: Date, required: true },
